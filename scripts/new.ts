51c027ec74120ecce61ed4f9b42c12fca7f05608
bsdayo <bsdayo@outlook.com>
2025-09-08 13:34:34 +0800

    scripts: use inquirer instead of commander

--- scripts/new.ts
+++ scripts/new.ts
@@ -1,23 +1,26 @@
-import { program } from 'commander'
-import dayjs from 'dayjs'
 import fs from 'node:fs'
+import fsp from 'node:fs/promises'
 import cp from 'node:child_process'
+import { input } from '@inquirer/prompts'
+import dayjs from 'dayjs'
 
-program.argument('<slug>', 'Post slug to create').parse()
+async function main() {
+  const slug = await input({ message: 'Enter slug' })
+  const created = dayjs().format('YYYY-MM-DD HH:mm:ss Z')
+  const content = `---\ntitle: \ntags: []\ncategory: \ncreated: ${created}\n---\n\n`
 
-const created = dayjs().format('YYYY-MM-DD HH:mm:ss Z')
-const content = `---\ntitle: \ntags: []\ncategory: \ncreated: ${created}\n---\n\n`
-const slug = program.args[0]
+  const year = dayjs().year()
+  const dir = `posts/${year}/${slug}`
 
-const year = dayjs().year()
-const dir = `posts/${year}/${slug}`
+  if (fs.existsSync(dir)) {
+    console.error(`Post with slug "${slug}" already exists.`)
+    process.exit(1)
+  }
 
-if (fs.existsSync(dir)) {
-  console.error(`Post with slug "${slug}" already exists.`)
-  process.exit(1)
+  await fsp.mkdir(dir, { recursive: true })
+  await fsp.writeFile(`${dir}/index.md`, content, { encoding: 'utf-8' })
+
+  cp.execSync(`code ${dir}/index.md`)
 }
 
-fs.mkdirSync(dir, { recursive: true })
-fs.writeFileSync(`${dir}/index.md`, content, { encoding: 'utf-8' })
-
-cp.execSync(`code ${dir}/index.md`)
+main()
