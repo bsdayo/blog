a7f1b49fc0dd61f23fdba7d548d09ae9ac9a553d
bsdayo <bsdayo@outlook.com>
2025-10-07 13:23:48 +0800

    feat: align post date to right side on mobile screen

--- components/PostLink.vue
+++ components/PostLink.vue
@@ -1,12 +1,12 @@
 <template>
   <li class="relative hoverable">
-    <a class="border-none flex gap-4 items-center" :href="withBase(`/posts/${post.slug}/`)">
+    <a
+      class="border-none flex justify-between sm:justify-start gap-4 items-center"
+      :href="withBase(`/posts/${post.slug}/`)"
+    >
       <span>{{ post.title }}</span>
       <span class="text-sm text-caption">
         {{ dayjs(post.created).format('MM-DD') }}
-      </span>
-      <span v-if="post.note" class="text-sm bg-zinc-500/10 rounded opacity-50 px-6px py-3px">
-        速记
       </span>
     </a>
   </li>
