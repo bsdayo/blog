8881d62bb0a8a043a2fab51dc92c70a089998217
bsdayo <bsdayo@outlook.com>
2025-10-14 09:17:19 +0800

    feat: show nav dropdown in mobile

--- src/components/HeaderActions.tsx
+++ src/components/HeaderActions.tsx
@@ -5,37 +5,86 @@ import {
   NavigationMenuLink,
 } from './ui/navigation-menu'
 import { Button } from './ui/button'
-import { Eclipse, Info, Newspaper, Notebook } from 'lucide-react'
+import { Eclipse, Info, Menu, Newspaper, Notebook } from 'lucide-react'
 import { toggleTheme } from '@/utils/theme'
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuShortcut,
+  DropdownMenuSeparator,
+} from './ui/dropdown-menu'
+import type { HTMLAttributeAnchorTarget, ReactNode } from 'react'
+
+interface HeaderLink {
+  icon: ReactNode
+  name: string
+  href: string
+  target?: HTMLAttributeAnchorTarget
+}
+
+const headerLinks: HeaderLink[] = [
+  {
+    icon: <Newspaper />,
+    name: '文章',
+    href: '/posts/',
+  },
+  {
+    icon: <Notebook />,
+    name: '速记',
+    href: 'https://memos.bsdayo.moe',
+    target: '_blank',
+  },
+  {
+    icon: <Info />,
+    name: '关于',
+    href: '/about/',
+  },
+]
 
 export function HeaderActions() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/posts/">
-            <span className="hidden sm:inline">文章</span>
-            <Newspaper className="sm:hidden" />
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="https://memos.bsdayo.moe" target="_blank">
-            <span className="hidden sm:inline">速记</span>
-            <Notebook className="sm:hidden" />
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuLink href="/about/">
-            <span className="hidden sm:inline">关于</span>
-            <Info className="sm:hidden" />
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="sm:hidden">
+          <Button size="icon" variant="ghost">
+            <Menu />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="z-200" collisionPadding={16}>
+          {headerLinks.map((link) => (
+            <DropdownMenuItem key={link.href} asChild>
+              <a href={link.href} target={link.target}>
+                {link.name}
+              </a>
+            </DropdownMenuItem>
+          ))}
+
+          <DropdownMenuSeparator />
+
           <Button size="icon" variant="ghost" onClick={() => toggleTheme()}>
             <Eclipse />
           </Button>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <NavigationMenu className="hidden sm:block">
+        <NavigationMenuList>
+          {headerLinks.map((link) => (
+            <NavigationMenuItem key={link.href}>
+              <NavigationMenuLink href={link.href} target={link.target}>
+                {link.name}
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          ))}
+          <NavigationMenuItem>
+            <Button size="icon" variant="ghost" onClick={() => toggleTheme()}>
+              <Eclipse />
+            </Button>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </>
   )
 }
